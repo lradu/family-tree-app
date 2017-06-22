6bfa31b5468d2f69fb567f5efdf5e40911c9e4f4
Radu Lucut <lucutradu@gmail.com>
2017-06-23 01:45:07 +0300

    replies

--- src/app/diagram/chat/chat.component.ts
+++ src/app/diagram/chat/chat.component.ts
@@ -9,11 +9,15 @@ import { FirebaseApp } from 'angularfire2';
 })
 
 export class ChatComponent implements AfterViewInit {
-    public dbref: any;
-    public user: any;
+    private dbref: any;
+    private user: any;
 
-    public chatId: string;
-    public messages: any = [];
+    private userData: any = {};
+    private chatId: string;
+    private showChatInput: boolean = false;
+    private messages: any = [];
+    private replies: any = [];
+    private currentReply: any = {};
 
     constructor(@Inject(FirebaseApp) firebase: any) {
         this.dbref = firebase.database().ref();
@@ -23,14 +27,45 @@ export class ChatComponent implements AfterViewInit {
     ngAfterViewInit() {
         // get the user's chat
         this.dbref
-            .child('users/' + this.user.uid + '/chat')
+            .child('users/' + this.user.uid)
             .once('value', (dataSnapshot) => {
+                let chat;
                 if(dataSnapshot.val()){
-                    this.getMessages(dataSnapshot.val());
+                    this.userData = dataSnapshot.val();
+                    chat = this.userData.chat;
+                }
+
+                if(chat){
+                    this.getMessages(chat);
+                    this.getSolvedTags(chat);
                 } else {
-                    this.createChat();
+                    this.getReplies(null)
+                        .then(() => {
+                           this.createChat(); 
+                        });
                 } 
         });
+    }
+
+    // create a new chat
+    createChat(){
+        return this.dbref
+            .child('chats')
+            .push({
+                user: this.user.uid
+            }).then((newChat) => {
+                this.dbref
+                    .child('users/' + this.user.uid)
+                    .update({
+                        chat: newChat.key
+                    });
+                console.log(this.replies);
+                this.getMessages(newChat.key);
+                this.getNextReply();
+                this.postSolvedTag(newChat.key, this.currentReply.tag);
+                this.getNextReply();
+                this.postSolvedTag(newChat.key, this.currentReply.tag);
+            });
     }
 
     // get chat messages
@@ -43,33 +78,96 @@ export class ChatComponent implements AfterViewInit {
         });
     }
 
-    // create a new chat
-    createChat(){
-        this.dbref
-            .child('chats')
-            .push({
-                members: this.user.uid
-            }).then((newChat) => {
-                this.dbref
-                    .child('users/' + this.user.uid)
-                    .update({
-                        chat: newChat.key
-                    });
-                this.getMessages(newChat.key);
-            });
-    }
-
-    sendMessage(event, message){
+    // save user input
+    postMessage(event, message){
         if(!event || !message || event.keyCode !== 13) return;
 
         let date = + new Date();
         this.dbref
             .child('messages/' + this.chatId)
             .push({
-                'name': this.user.displayName || 'Anonymous',
+                'name': this.userData.name || 'Anonymous',
                 'message': message,
                 'timestamp': date
             });
+
+        this.userData[this.currentReply.tag] = message;
+
+        this.postTag(message)
+            .then(() => {
+                this.postSolvedTag(this.chatId, this.currentReply.tag)
+                    .then(() => {
+                        this.getNextReply();
+                    });
+            });
+
         event.srcElement.value = '';
     }
+
+    // check entity
+    checkEntity(){
+
+    }
+
+    // add tag to entity
+    postTag(res){
+        return this.dbref
+            .child('users/' + this.user.uid)
+            .update({
+                [this.currentReply.tag]: res
+            });
+    }
+
+    // add tag to solved
+    postSolvedTag(chatId, tag){
+        return this.dbref
+            .child('chats/' + chatId + '/solvedTags')
+            .update({
+                [tag]: true
+            });
+    }
+
+    // get solved replies tags
+    getSolvedTags(chatId){
+        return this.dbref
+            .child('chats/' + chatId + '/solvedTags')
+            .once('value', (snapShot) => {
+                this.getReplies(snapShot.val());
+            });
+    }
+
+    // get replies
+    getReplies(solvedTags) {
+        return this.dbref
+            .child('replies')
+            .orderByChild('priority')
+            .once('value', (snapShot) => {
+                if(solvedTags){
+                    snapShot.forEach((reply) => {
+                        if(!solvedTags[reply.val().tag]){
+                            this.replies.push(reply.val());
+                        }
+                    });
+                } else {
+                    snapShot.forEach((reply) => {
+                        this.replies.push(reply.val());
+                    });
+                }
+            }).then(() => {
+                this.showChatInput = true;
+            });
+    }
+
+    getNextReply(){
+        if(!this.replies.length) return;
+
+        let date = + new Date();
+        this.replies[0].timestamp = date;
+        this.currentReply = this.replies[0];
+        this.replies.shift();
+
+        this.dbref
+            .child('messages/' + this.chatId)
+            .push(this.currentReply);
+    }   
 }
--- src/app/diagram/diagram.component.ts
+++ src/app/diagram/diagram.component.ts
@@ -40,16 +40,11 @@ export class DiagramComponent implements AfterViewInit {
         this.gRelationships = this.svg.append("g")
             .attr("class", "layer relationships");
         
-        let node = new Node();
-        this.render({
-            nodes: {
-                firstNode: node
-            }
-        });
+        this.getData();
     }
 
     // Render diagram
-    render(data){
+    render(data) {
         let nodes, relationships;
 
         this.diagram = new Diagram();
@@ -62,6 +57,64 @@ export class DiagramComponent implements AfterViewInit {
 
         relationships = new RenderRelationships();
         relationships.render(this.gRelationships, this.diagram.relationships);
+    }
+
+    getData() {
+
+        this.dbref
+            .child('users/' + this.user.uid)
+            .on('value', (snapShot) => {
+                if(snapShot.val()) {
+                    this.handleData(snapShot.val());
+                }
+            });
+    }
+
+    handleData(data) {
+        let node = new Node();
+        let size, center;
+
+        if(data.name) {
+            size = this.getTxtLength(data.name);
+            node.radius = size;
+            node.caption = data.name;
+        };
+        if(data.age) {
+            size = this.getTxtLength(data.name);
+            node.propertiesWidth = size;
+            node.properties = data.age;
+        };
+
+        center = this.getSvgCenter();
+        node.x = center[0] - node.radius;
+        node.y = center[1] - node.radius;
+
+        this.render({
+            nodes: {
+                firstNode: node
+            }
+        });
+    }
+
+    getTxtLength(text) {
+        let txt = this.svg.append("text")
+            .attr("font-size",  "50px")
+            .text(text);
+        let size = txt.node().getComputedTextLength() / 2 + 20;
+        txt.remove();
+
+        return size < 50 ? 50:size;
+    }
+
+    getSvgCenter() {
+        let svg = this.svg.node();
+
+        let fullWidth = svg.clientWidth || svg.parentNode.clientWidth,
+            fullHeight = svg.clientHeight || svg.parentNode.clientHeight;
+        let halfWidth = fullWidth / 2,
+            halfHeight = fullHeight / 2;
+
+        return [halfWidth, halfHeight];
     }
 
 }
